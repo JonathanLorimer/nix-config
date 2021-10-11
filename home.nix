1edf8de2c597e79dff84a62e8f2cdb4691e4b7a2
Jonathan Lorimer <jonathan_lorimer@mac.com>
2021-10-11 12:25:16 -0400

    update home-manager

--- home.nix
+++ home.nix
@@ -544,8 +544,8 @@ in {
           set -g status-fg white
           set -g status-style fg=white,bg=default
 
-          set -g status-left \'\'
-          set -g status-right \'\'
+          set -g status-left ${"''"}
+          set -g status-right ${"''"}
           set -g status-justify centre
           set -g status-position bottom
 
@@ -555,10 +555,6 @@ in {
           set -g window-status-current-format "#[fg=cyan]#[fg=black]#[bg=cyan]#I #[bg=brightblack]#[fg=white] #W#[fg=brightblack]#[bg=default] #[bg=default] #[fg=magenta]#[fg=black]#[bg=magenta]λ #[fg=white]#[bg=brightblack] %a %d %b #[fg=magenta]%R#[fg=brightblack]#[bg=default] "
           set -g window-status-format "#[fg=magenta]#[fg=black]#[bg=magenta]#I #[bg=brightblack]#[fg=white] #W#[fg=brightblack]#[bg=default] "
         '';
-      };
-      obs-studio = {
-        enable = true;
-        plugins = with pkgs.obs-studio-plugins; [ wlrobs ];
       };
     };
     services = {
