1d239d08de51a05fdda166e23e6cdf8c0edcbdc1
Jonathan Lorimer <jonathan_lorimer@mac.com>
2023-05-03 11:58:09 -0400

    make everything minimal so audio works

--- modules/pipewire.nix
+++ modules/pipewire.nix
@@ -1,10 +1,9 @@
 {pkgs, ...}: {
   xdg.portal.enable = true;
   xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-wlr];
-  services.pipewire.enable = true;
-  services.pipewire.audio.enable = true;
-  services.pipewire.wireplumber.enable = true;
-  services.pipewire.alsa.enable = true;
-  services.pipewire.jack.enable = true;
-  services.pipewire.pulse.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+  };
 }
