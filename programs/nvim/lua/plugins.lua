2e218e9e1d4aab1edb0129f3e079ee4c4f795137
Jonathan Lorimer <jonathan_lorimer@mac.com>
2023-11-17 18:27:19 -0500

    add terraform comments

--- programs/nvim/lua/plugins.lua
+++ programs/nvim/lua/plugins.lua
@@ -3,6 +3,7 @@ require 'colorizer'.setup { user_default_options = { css = true, }, }
 require 'Comment'.setup()
 local ft = require('Comment.ft')
 ft.agda = { '--%s', '{-%s-}' }
+ft.tf = { '#%s', '{/*%s*/}' }
 require 'todo-comments'.setup {}
 require 'fidget'.setup {}
 require 'crates'.setup()
